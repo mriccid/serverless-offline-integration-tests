f97cf54b4f48efebb476e7181e5181a8cc18036c
Michael Ricci <mriccid@gmail.com>
2019-03-23 17:01:51 +1100

    Add integration tests

---
+++ test/handler.spec.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+const request = require('supertest');
+
+const api = request('http://localhost:3000');
+
+describe('User API', function() {
+
+	describe('GET /user/{id}', function() {
+		it('should return a 404 when requested without an id', function(done) {
+			api.get('/user')
+				.expect(404)
+				.end(done)
+		})
+
+		it('should return a 404 when given an id that does not match', function(done) {
+			api.get('/user/_1')
+				.expect(404)
+				.end(done)
+		})
+
+		it('should return an existing user if given their id', function(done) {
+			api.get('/user/1')
+				.expect({id: '1', name: 'Foo'})
+				.expect(200)
+				.end(done)
+		})
+
+	})
+
+	describe('POST /user', function() {
+		it('should return a 400 when requested without a payload', function(done) {
+			api.post('/user')
+				.expect(400)
+				.expect({message: 'Missing body'})
+				.end(done)
+		})
+		
+		it('should return a 400 when adding a user without a name', function(done) {
+			api.post('/user')
+				.send({ notName: '1234' })
+				.expect(400)
+				.expect({message: 'Expected a name'})
+				.end(done)
+		})
+
+		// @todo setup persistence
+		// it('should add a user to the list', async function(done) {
+		// 	await api.post('/user')
+		// 		.send({ name: 'Baz' })
+		// 		.expect(200);
+
+		// 	// @todo test the persistence of user
+
+		// })
+	})
+
+});
